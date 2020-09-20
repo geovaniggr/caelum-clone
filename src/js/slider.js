74726cd96acf10f977c125a720570e1a4b7ef57c
Geovani Granieri <geovani.hilario@outlook.com>
2020-09-20 14:46:03 -0300

    feat: slider now has animation & timer for mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,6 +3,7 @@
     let index = 0;
     let widthOfTranslate = 0;
     let timeout = 0;
+    let interval = 0;
     const delay = 300;
     const images = document.querySelectorAll('.instructors__card');
     const nextButton = document.querySelector('.instructors__slider--next');
@@ -10,14 +11,21 @@
     const getClientWidth = () => {
         const firstImageOfCard = images[0].querySelector('.instructors__photo');
         widthOfTranslate = firstImageOfCard.width;
+        if (widthOfTranslate > 250) {
+            clearInterval(interval);
+        }
+        else {
+            interval = setInterval(nextImage, 5000);
+        }
     };
     const translate = (element) => element.style.transform = `translateX(${(-index * widthOfTranslate)}px)`;
-    nextButton.addEventListener('click', () => {
+    const nextImage = () => {
         index++;
         if (index >= images.length)
             index = 0;
         images.forEach(translate);
-    });
+    };
+    nextButton.addEventListener('click', nextImage);
     previousButton.addEventListener('click', () => {
         index--;
         if (index < 0)
--- src/scripts/slider.ts
+++ src/scripts/slider.ts
@@ -1,7 +1,8 @@
 (function () {
     let index = 0;
     let widthOfTranslate = 0;
-    let timeout: number = 0;
+    let timeout =  0;
+    let interval = 0;
     const delay = 300;
 
     const images = document.querySelectorAll('.instructors__card') as NodeListOf<HTMLElement>;
@@ -11,22 +12,31 @@
     const getClientWidth = () => {
         const firstImageOfCard =  images[0].querySelector('.instructors__photo') as HTMLImageElement;
         widthOfTranslate = firstImageOfCard.width;
+
+        if(widthOfTranslate > 250) {
+            clearInterval(interval)
+        } else {
+            interval = setInterval(nextImage, 5000)
+        }
    }
 
     const translate = (element: HTMLElement) => element.style.transform = `translateX(${(-index * widthOfTranslate)}px)`;
 
-    nextButton.addEventListener('click', () => {
+    const nextImage = () => {
         index++;
         if (index >= images.length)
             index = 0;
         images.forEach(translate)
-    });
+    }
 
+    nextButton.addEventListener('click', nextImage); 
+    
     previousButton.addEventListener('click', () => {
         index--;
         if (index < 0)
             index = images.length - 1;
         images.forEach(translate);
+        
     });
 
     window.addEventListener('resize',  () => {
